de88b82ae681fb31f5c6b6eb238a66595d30e6b7
guima <guilherme101198@icloud.com>
2023-12-16 12:21:12 -0500

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        val nav_version = "2.5.3"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath ("com.android.tools.build:gradle:7.0.0")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    }
+
+}
